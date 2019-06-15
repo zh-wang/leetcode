bed27f12abe25ec7f1a65015a092cb7dd7885ad5
zh-wang <viennakanon@gmail.com>
2019-06-15 12:36:25 +0900

    0042 two pointers

---
+++ solutions/0042_Trapping_Rain_Water/trick_two_pointers.py
@@ -0,0 +1,18 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        l, r = 0, len(height) - 1
+        l_max, r_max, ret = 0, 0, 0
+        while l < r:
+            if height[l] < height[r]:
+                if height[l] < l_max:
+                    ret += l_max - height[l]
+                else:
+                    l_max = height[l]
+                l += 1
+            else:
+                if height[r] < r_max:
+                    ret += r_max - height[r]
+                else:
+                    r_max = height[r]
+                r -= 1
+        return ret
