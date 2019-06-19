7442cb92aa1c2394bc1e1bb4ab61007dec9a31c7
zh-wang <viennakanon@gmail.com>
2019-06-19 15:39:59 +0900

    0046+

--- solutions/0046_Permutations/dfs_modify_input_array.py
+++ solutions/0046_Permutations/dfs_modify_input_array.py
@@ -1,17 +1,18 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         self.ret = []
-        self.l = []
+        # no need an additional list to store temp answer
+        # temp anwser will be filled in nums
         self.recur(nums, 0)
         return self.ret
 
     def recur(self, nums, start_index):
+        # the left of nums before start_index will always be a composed perm
+        # the right will always be a working list
         if start_index >= len(nums):
-            self.ret.append(self.l[:])
+            self.ret.append(nums[:])
             return
         for i in range(start_index, len(nums)):
-            self.l.append(nums[i])
             nums[start_index], nums[i] = nums[i], nums[start_index]
             self.recur(nums, start_index + 1)
             nums[start_index], nums[i] = nums[i], nums[start_index]
-            self.l.pop()
