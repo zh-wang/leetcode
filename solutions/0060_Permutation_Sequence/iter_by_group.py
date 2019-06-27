917977983823a022ec2ee5e93cebc10cf510734f
zh-wang <viennakanon@gmail.com>
2019-06-27 17:20:30 +0900

    0060

--- solutions/0060_Permutation_Sequence/iter_by_group.py
+++ solutions/0060_Permutation_Sequence/iter_by_group.py
@@ -5,12 +5,12 @@ class Solution:
         ret = 0
         nums = [i+1 for i in range(n)]
         total = reduce((lambda x, y: x * y), nums)
-        span = n
-        while k >= 0 and span > 0:
-            total //= span
-            i = k // total
+        numOfGroup = n
+        while k >= 0 and numOfGroup > 0:
+            total //= numOfGroup # num of slots in each group
+            i = k // total # the index of group
             ret = ret * 10 + nums[i]
             del nums[i]
-            k = k % total
-            span -= 1
+            k = k % total # the remaining k for next loop
+            numOfGroup -= 1 # the next num of group
         return str(ret)
