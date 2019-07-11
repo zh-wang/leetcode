7dcbeb72c541e9de80285687f601a8549b504a07
zh-wang <viennakanon@gmail.com>
2019-07-11 18:41:34 +0900

    update 0033

--- solutions/0033_Search_in_Rotated_Sorted_Array/binary_search.py
+++ solutions/0033_Search_in_Rotated_Sorted_Array/binary_search.py
@@ -2,25 +2,20 @@ class Solution:
     def search(self, nums: List[int], target: int) -> int:
         if len(nums) == 0:
             return -1
-        ret = self.bi_search(nums, target, 0, len(nums) - 1)
-        return ret
+        l, r = 0, len(nums) - 1
+        while l < r:
+            m = (l + r) // 2
+            if nums[m] == target:
+                return m
+            if nums[l] < nums[m]: # left part is sorted
+                if nums[l] <= target <= nums[m]: # answer in the left
+                    r = m
+                else: # otherwise, search in the right
+                    l = m + 1
+            else: # right part is sorted
+                if nums[m+1] <= target <= nums[r]:
+                    l = m + 1
+                else:
+                    r = m
+        return l if nums[l] == target else -1
 
-    def bi_search(self, nums, target, l, r):
-        if l == r:
-            if nums[l] == target:
-                return l
-            else:
-                return - 1
-        m = (l + r) // 2
-        if nums[m] == target:
-            return m
-        if nums[l] < nums[m]: # left part is sorted
-            if nums[l] <= target <= nums[m]: # answer in the left
-                return self.bi_search(nums, target, l, m)
-            else: # otherwise, search in the right
-                return self.bi_search(nums, target, m+1, r)
-        else: # right part is sorted
-            if nums[m+1] <= target <= nums[r]:
-                return self.bi_search(nums, target, m+1, r)
-            else:
-                return self.bi_search(nums, target, l, m)
