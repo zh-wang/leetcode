455570974dda014f885028e103874457d9ed65a7
zh-wang <viennakanon@gmail.com>
2019-07-13 22:04:58 +0900

    0084

---
+++ solutions/0084_Largest_Rectangle_in_Histogram/min_segment_tree.py
@@ -0,0 +1,48 @@
+from math import log2, ceil
+from typing import *
+
+class MinSegTree:
+
+    NO_VALUE = 1<<31
+
+    def __init__(self, nums):
+        self.height = ceil(log2(len(nums))) + 1
+        self.n = len(nums)
+        self.max_size = 2**self.height - 1
+        self.nodes = [(MinSegTree.NO_VALUE, -1)] * self.max_size
+        self.build(nums, 0, len(nums) - 1, 0)
+
+    def build(self, nums, l, r, i):
+        # params => array, left bound, right bound, index of current node
+        # print(l, r, i)
+        if l == r: # If there is only one element
+            self.nodes[i] = (nums[l], l)
+            return self.nodes[i]
+        mid = (l + r) // 2
+        self.nodes[i] = min(self.build(nums, l, mid, i * 2 + 1), self.build(nums, mid+1, r, i * 2 + 2))
+        return self.nodes[i]
+
+    def query_min(self, ql, qr):
+        return self._min(ql, qr, 0, self.n - 1, 0)
+
+    def _min(self, ql, qr, l, r, i):
+        if ql <= l and qr >= r:
+            return self.nodes[i]
+        if ql > r or qr < l:
+            return (MinSegTree.NO_VALUE, -1)
+        mid = (l + r) // 2
+        return min(self._min(ql, qr, l, mid, i*2+1), self._min(ql, qr, mid+1, r, i*2+2))
+
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        if len(heights) == 0: return 0
+        st = MinSegTree(heights)
+        return self.max_area(st, heights, 0, len(heights)-1)
+
+    def max_area(self, st, heights, l, r):
+        if l > r: return -1
+        if l == r: return heights[l]
+        h, m = st.query_min(l, r)
+        return max(self.max_area(st, heights, l, m-1), \
+                self.max_area(st, heights, m+1, r), \
+                (r-l+1) * h)
