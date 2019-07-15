dce90b4e0d4e82a249beb8c0c81a8103b526b4d4
zh-wang <viennakanon@gmail.com>
2019-07-15 10:59:57 +0900

    0085

--- solutions/0085_Maximal_Rectangle/recur_as_0084.java
+++ solutions/0085_Maximal_Rectangle/recur_as_0084.java
@@ -1,5 +1,22 @@
 class Solution {
+
     int best = 0;
+
+    public int maximalRectangle(char[][] matrix) {
+        if (matrix == null || matrix.length == 0) return 0;
+        if (matrix[0] == null || matrix[0].length == 0) return 0;
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[] hist = new int[n];
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                int v = matrix[i][j] == '1' ? 1 : 0;
+                hist[j] = v == 1 ? hist[j] + 1 : 0;
+            }
+            best = Math.max(best, largestRectangleArea(hist));
+        }
+        return best;
+    }
 
     public int largestRectangleArea(int[] heights) {
         best = 0;
@@ -29,4 +46,4 @@ class Solution {
             }
         }
     }
-}
+    }
