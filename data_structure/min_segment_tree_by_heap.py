7fb140fd259a7bb347e638bf90d433364c6259c9
zh-wang <viennakanon@gmail.com>
2019-07-16 11:56:12 +0900

    data_struct

--- data_structure/min_segment_tree_by_heap.py
+++ data_structure/min_segment_tree_by_heap.py
@@ -8,31 +8,32 @@ class MinSegTree:
         self.height = ceil(log2(len(nums))) + 1
         self.n = len(nums)
         self.max_size = 2**self.height - 1
-        self.nodes = [MinSegTree.NO_VALUE] * self.max_size
+        # `nodes` stores (min value, index of the min value in `nums`)
+        self.nodes = [(MinSegTree.NO_VALUE, -1)] * self.max_size
         self.build(nums, 0, len(nums) - 1, 0)
 
     def build(self, nums, l, r, i):
         # params => array, left bound, right bound, index of current node
-        print(l, r, i)
+        # print(l, r, i)
         if l == r: # If there is only one element
-            self.nodes[i] = nums[l]
+            self.nodes[i] = (nums[l], l)
             return self.nodes[i]
         mid = (l + r) // 2
         self.nodes[i] = min(self.build(nums, l, mid, i * 2 + 1), self.build(nums, mid+1, r, i * 2 + 2))
         return self.nodes[i]
 
     def query_min(self, ql, qr):
-        return self._min(ql, qr, 0, self.n, 0)
+        return self._min(ql, qr, 0, self.n - 1, 0)
 
     def _min(self, ql, qr, l, r, i):
-        print(l, r, i)
+        # print(l, r, i)
         if ql <= l and qr >= r:
             return self.nodes[i]
         if ql > r or qr < l:
-            return MinSegTree.NO_VALUE
+            return (MinSegTree.NO_VALUE, -1)
         mid = (l + r) // 2
         return min(self._min(ql, qr, l, mid, i*2+1), self._min(ql, qr, mid+1, r, i*2+2))
 
 st = MinSegTree([1,2,3,4,3,2,1])
 print(st.nodes)
-print(st.query_min(1,3))
+print(st.query_min(0, 6))
---
+++ data_structure/min_segment_tree_by_heap2.py
@@ -0,0 +1,43 @@
+from math import log2, ceil
+
+class MinSegTree:
+
+    NO_INDEX = -1
+    NO_VALUE = 1<<31
+
+    def __init__(self, nums):
+        self.height = ceil(log2(len(nums))) + 1
+        self.n = len(nums)
+        self.nums = nums;
+        self.max_size = 2**self.height - 1
+        # `nodes` stores index of the min value in `nums`
+        self.nodes = [MinSegTree.NO_INDEX] * self.max_size
+        self.build(nums, 0, len(nums) - 1, 0)
+
+    def build(self, nums, l, r, i):
+        # params => array, left bound, right bound, index of current node
+        # print(l, r, i)
+        if l == r: # If there is only one element
+            self.nodes[i] = l
+            return l
+        mid = (l + r) // 2
+        li = self.build(nums, l, mid, i*2+1)
+        ri = self.build(nums, mid+1, r, i*2+2)
+        self.nodes[i] = li if self.nums[li] < self.nums[ri] else ri
+        return self.nodes[i]
+
+    def query_min(self, ql, qr):
+        return self._min(ql, qr, 0, self.n - 1, 0)
+
+    def _min(self, ql, qr, l, r, i):
+        # print(l, r, i)
+        if ql <= l and qr >= r:
+            return self.nums[self.nodes[i]] if self.nodes[i] > 0 else MinSegTree.NO_VALUE
+        if ql > r or qr < l:
+            return MinSegTree.NO_VALUE
+        mid = (l + r) // 2
+        return min(self._min(ql, qr, l, mid, i*2+1), self._min(ql, qr, mid+1, r, i*2+2))
+
+st = MinSegTree([1,2,3,4,3,2,1])
+print(st.nodes)
+print(st.query_min(4, 5))
