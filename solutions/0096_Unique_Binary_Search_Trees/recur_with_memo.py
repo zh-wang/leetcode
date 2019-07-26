86105115b034afa74517553ed7c7e7b4494cec4d
zh-wang <viennakanon@gmail.com>
2019-07-26 11:12:13 +0900

    0096

--- solutions/0096_Unique_Binary_Search_Trees/recur_with_memo.py
+++ solutions/0096_Unique_Binary_Search_Trees/recur_with_memo.py
@@ -10,8 +10,6 @@ class Solution:
             return dp[j-i]
         if i == j:
             dp[j-i] = 1
-        elif i+1 == j:
-            dp[j-i] = 2
         else:
             for k in range(i, j+1):
                 dp[j-i] += self.recur(dp, i, k-1) * self.recur(dp, k+1, j)
