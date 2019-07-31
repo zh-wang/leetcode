93aae6317b6d3a41f50e6994bb0841ce73e1fa65
zh-wang <viennakanon@gmail.com>
2019-07-31 15:30:49 +0900

    0101

---
+++ solutions/0101_Symmetric_Tree/recur.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return isSymmetric(root.left, root.right);
+    }
+
+    public boolean isSymmetric(TreeNode t1, TreeNode t2) {
+        if (t1 == null && t2 == null) {
+            return true;
+        } else if (t1 != null && t2 != null) {
+            return t1.val == t2.val && isSymmetric(t1.left, t2.right) && isSymmetric(t1.right, t2.left);
+        } else {
+            return false;
+        }
+    }
+}
