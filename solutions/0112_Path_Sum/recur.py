a6bf930e8d039984e724faffd0c1caf2725d51af
zh-wang <viennakanon@gmail.com>
2019-08-02 20:07:21 +0900

    0112

---
+++ solutions/0112_Path_Sum/recur.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def hasPathSum(self, root: TreeNode, sum: int) -> bool:
+        if not root:
+            return False
+        if not root.left and not root.right:
+            return True if sum == root.val else False
+        elif not root.left:
+            return self.hasPathSum(root.right, sum - root.val)
+        elif not root.right:
+            return self.hasPathSum(root.left, sum - root.val)
+        else:
+            return self.hasPathSum(root.left, sum - root.val) or \
+                   self.hasPathSum(root.right, sum - root.val)
