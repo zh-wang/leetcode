8de98771d513834e43ebb1f161f62377c95b8a2e
zh-wang <viennakanon@gmail.com>
2019-08-04 23:51:02 +0900

    0116

---
+++ solutions/0116_Populating_Next_Right_Pointers_in_Each_Node/recur.py
@@ -0,0 +1,21 @@
+// ⭐️
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val, left, right, next):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        self.recur(root, None)
+        return root
+
+    def recur(self, cur, tar):
+        if not cur:
+            return
+        cur.next = tar
+        self.recur(cur.left, cur.right)
+        self.recur(cur.right, cur.next.left if cur.next else None)
