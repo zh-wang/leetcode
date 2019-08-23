4dda425498d06b8da2df2c4e1af865362865ceea
zh-wang <viennakanon@gmail.com>
2019-08-23 14:35:53 +0900

    0094

---
+++ solutions/0094_Binary_Tree_Inorder_Traversal/double_stacks.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> ret = new ArrayList<>();
+        if (root == null) return ret;
+        Stack<TreeNode> rChilds = new Stack<>();
+        Stack<Integer> centers = new Stack<>();
+        centers.push(root.val);
+        TreeNode cur = root; // always keep left child
+        while (cur != null) {
+            centers.push(cur.val);
+            rChilds.push(cur.right);
+            cur = cur.left; // recur on left child
+            if (cur == null) { // no left child
+                // pop until we found a valid right child as cur
+                while (rChilds.size() > 0 && cur == null) {
+                    ret.add(centers.pop());
+                    cur = rChilds.pop();
+                }
+            }
+        }
+        return ret;
+    }
+}
