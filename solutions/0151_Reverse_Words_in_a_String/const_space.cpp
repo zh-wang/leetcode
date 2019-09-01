8311c51c578011716ffb83a93d318ca19cf71afd
zh-wang <viennakanon@gmail.com>
2019-09-01 18:24:33 +0900

    0151

---
+++ solutions/0151_Reverse_Words_in_a_String/const_space.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        reverse(s, 0, s.length() - 1);
+
+        int i = 0, j = 0; // start & end for each word
+        int p = 0; // start position for lShift
+        while (i < s.length()) {
+            while (i < s.length() && s[i] == ' ') ++i;
+            if (s[i] == '\0') break;
+            j = i;
+            while (j < s.length() && s[j] != ' ') ++j;
+            // printf("%d, %d\n", i, j);
+            reverse(s, i, j - 1);
+            if (p > 0) {
+                s[p] = ' ';
+                ++p;
+            }
+            p += lShift(s, p, i, j - 1);
+            i = j + 1;
+        }
+        s.resize(p);
+        return s;
+    }
+
+    void reverse(string &s, int i, int j) {
+        while (i < j) {
+            char c = s[i];
+            s[i] = s[j];
+            s[j] = c;
+            ++i, --j;
+        }
+    }
+
+    int lShift(string &s, int p, int i, int j) {
+        if (p == i) return j - i + 1;
+        int k = i;
+        while (k <= j) {
+            s[p] = s[k];
+            ++p, ++k;
+        }
+        return j - i + 1;
+    }
+};
