7fe48c56dd3306ba0c77243750023fcebf720545
zh-wang <viennakanon@gmail.com>
2019-10-07 10:58:37 +0900

    0280_google

---
+++ solutions/0280_Wiggle_Sort/sort_and_swap_NLogN.java
@@ -0,0 +1,23 @@
+// --Link--
+// [LintCode](https://www.lintcode.com/problem/wiggle-sort/description)
+//
+public class Solution {
+    /*
+     * @param nums: A list of integers
+     * @return: nothing
+     */
+    public void wiggleSort(int[] nums) {
+        // write your code here
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length / 2; ++i) {
+            int temp = nums[i];
+            nums[i] = nums[nums.length - 1 - i];
+            nums[nums.length - 1 - i] = temp;
+        }
+        for (int i = 1; i < nums.length; i += 2) {
+            int temp = nums[i - 1];
+            nums[i - 1] = nums[i];
+            nums[i] = temp;
+        }
+    }
+}
