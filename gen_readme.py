308d989f3d7b54cd752f09471452a477cb24932a
zh-wang <viennakanon@gmail.com>
2019-11-11 15:28:37 +0900

    update gen readme

--- gen_readme.py
+++ gen_readme.py
@@ -13,27 +13,42 @@ if __name__ == "__main__":
         for prob_dir in sorted(glob.glob("./solutions/*"), key=lambda x:os.path.basename(x)): # for each prob
             prob_desc = os.path.split(prob_dir)[1]
             print(os.path.basename(prob_dir))
-            solutions = []
             of.write("| ")
+            solutions = []
+            has_link = False # has a LintCode link or not
+            link = None # the LintCode url
+            has_prob = False # has a problem definition text
             for sol in glob.glob(prob_dir + "/*"): # for each solution
                 is_favo = False
                 try:
                     in_file = open(sol, "r")
                     first_line = in_file.readline()
-                    is_favo = '⭐️' in first_line
-                    has_link = 'Link' in first_line
-                    link = in_file.readline()[3:-1]
+                    is_favo = '⭐️' in first_line or 'STAR' in first_line
+                    if 'Link' in first_line:
+                        has_link = True
+                        link = in_file.readline()[3:-1]
+                    if os.path.basename(sol) == 'prob.md':
+                        has_prob = True
                 except Exception as e:
                     print(e)
                 finally:
                     in_file.close()
-                fn = os.path.split(sol)[1]
-                sol = "[%s](%s)%s" % (os.path.split(sol)[1], "%s%s/%s" % (GITHUB_FILE_LINK_PREFIX, prob_desc, fn), ' ⭐️' if is_favo else '')
-                solutions.append(sol)
+                fn = os.path.split(sol)[1] # file name
+                sol_github_entry = "[%s](%s)%s" % (
+                        fn,
+                        "%s%s/%s" % (GITHUB_FILE_LINK_PREFIX, prob_desc, fn),
+                        ' ⭐️' if is_favo else '')
+                if os.path.basename(sol) != 'prob.md':
+                    solutions.append(sol_github_entry)
+            # --- First column ---
+            # Problem's Index and Name
             of.write(prob_desc)
+            # Append link of prob.md
+            if has_prob:
+                of.write(' [prob.md](%s)' % ("%s%s/prob.md" % (GITHUB_FILE_LINK_PREFIX, prob_desc)))
+            # Append LinkCode link
             if has_link:
-                of.write(' => ')
-                of.write(link)
+                of.write(' [LintCode](%s)' % link)
             of.write(" | ")
             of.write(" <br> ".join(solutions))
             of.write(" |\n")
