bc9371f965e516144c845ce89066f253df068a86
zh-wang <viennakanon@gmail.com>
2019-11-20 13:58:08 +0900

    0209

---
+++ solutions/0209_Minimum_Size_Subarray_Sum/two_pointers.kt
@@ -0,0 +1,16 @@
+class Solution {
+    fun minSubArrayLen(s: Int, nums: IntArray): Int {
+        if (nums.size == 0) return 0
+        var sum = 0
+        var start = 0
+        var ret = nums.size
+        for (i in 0..nums.size-1) {
+            sum += nums[i]
+            while (sum >= s) {
+                ret = Integer.min(ret, i - start + 1)
+                sum -= nums[start++]
+            }
+        }
+        return ret
+    }
+}
