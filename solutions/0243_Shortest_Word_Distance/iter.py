b5603e57dc4f3ea1bff00e1a2a4b9ef2308f9b74
zh-wang <viennakanon@gmail.com>
2020-02-04 22:49:51 +0900

    0243

---
+++ solutions/0243_Shortest_Word_Distance/iter.py
@@ -0,0 +1,22 @@
+// --Link--
+// https://www.lintcode.com/problem/shortest-word-distance/description
+
+class Solution:
+    """
+    @param words: a list of words
+    @param word1: a string
+    @param word2: a string
+    @return: the shortest distance between word1 and word2 in the list
+    """
+    def shortestDistance(self, words, word1, word2):
+        # Write your code here
+        best = len(words)
+        p1, p2 = -len(words), -len(words)
+        for i, w in enumerate(words):
+            if w == word1:
+                p1 = i
+                best = min(best, p1 - p2)
+            if w == word2:
+                p2 = i
+                best = min(best, p2 - p1)
+        return best
