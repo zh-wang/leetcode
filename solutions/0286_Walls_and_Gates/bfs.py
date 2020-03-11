d27ce1ed92e84ed4187c5ca73f93886d2aebf0b9
zh-wang <viennakanon@gmail.com>
2020-03-11 22:28:17 +0900

    0286

---
+++ solutions/0286_Walls_and_Gates/bfs.py
@@ -0,0 +1,23 @@
+class Solution:
+    """
+    @param rooms: m x n 2D grid
+    @return: nothing
+    """
+    def wallsAndGates(self, rooms):
+        # write your code here
+        import queue
+        INF = (1 << 31) - 1
+        ds = [ [1, 0], [0, -1], [-1, 0], [0, 1] ]
+        q = queue.Queue()
+        r, c = len(rooms), len(rooms[0])
+        zeros = [(i, j, 0) for i in range(r) for j in range(c) if rooms[i][j] == 0]
+        for v in zeros:
+            q.put(v)
+        while q.qsize() > 0:
+            cur = q.get()
+            for d in ds:
+                ni, nj = cur[0] + d[0], cur[1] + d[1]
+                if 0 <= ni < r and 0 <= nj < c and rooms[ni][nj] == INF:
+                    rooms[ni][nj] = cur[2] + 1
+                    q.put((ni, nj, cur[2] + 1))
+        return
