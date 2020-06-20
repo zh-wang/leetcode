031c4b20b619d229523fb7284c6c135593b8ad27
zh-wang <viennakanon@gmail.com>
2020-06-20 10:06:22 +0900

    1288

--- solutions/1288_Remove_Covered_Intervals/line_sweep.java
+++ solutions/1288_Remove_Covered_Intervals/line_sweep.java
@@ -1,17 +1,20 @@
 class Solution {
     public int removeCoveredIntervals(int[][] intervals) {
         if (intervals.length == 0) return 0;
+        // sort intervals by their start point in ascending order
+        // if they are equal, sort by their end point in descending order
         Arrays.sort(intervals, (i1, i2) -> {
             return i1[0] == i2[0] ? i2[1] - i1[1] : i1[0] - i2[0];
         });
 
         int ret = 0;
-        int end = intervals[0][1];
+        int end = intervals[0][1]; // an overall end point
         for (int i = 1; i < intervals.length; ++i) {
-            if (intervals[i][1] <= end) {
+            // no need to check start point because they are in ascending sort
+            if (intervals[i][1] <= end) { // found overlap
                 ++ret;
             } else {
-                end = intervals[i][1];
+                end = intervals[i][1]; // update end point
             }
         }
         return intervals.length - ret;
