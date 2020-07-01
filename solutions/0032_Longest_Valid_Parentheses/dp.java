198b12501a7f9706258c6e822900f81bb39f9250
zh-wang <viennakanon@gmail.com>
2020-07-02 00:08:37 +0900

    0032

--- solutions/0032_Longest_Valid_Parentheses/dp.java
+++ solutions/0032_Longest_Valid_Parentheses/dp.java
@@ -1,23 +1,28 @@
 class Solution {
     public int longestValidParentheses(String s) {
+        if (s.length() <= 1) {
+            return 0;
+        }
         int best = 0;
-        int[] dp = new int[s.length() + 1]; // ith in dp => (i-1)th in s
-        dp[0] = 0;
-        for (int i = 1; i < s.length() + 1; ++i) {
-            if (s.charAt(i - 1) == '(') { // str ends with '(' is always not satisfied
-                dp[i] = 0;
-            } else { // str ends with ')'
-                if (i - 1 - 1 >= 0 && s.charAt(i - 1 - 1) == '(') { // ends with '()'
-                    dp[i] = dp[i - 2] + 2;
+        int[] dp = new int[s.length()];
+        for (int i = 1; i < s.length(); ++i) {
+            // dp[i] = 0                        if s[i] == '('
+            //       = dp[i-2] + 2              if s[i] == ')' and s[i-1] == '('
+            //                                  (let pi = i - dp[i-1] - 1)
+            //       = dp[i-1] + dp[pi-1] + 2    if s[i] == ')' and s[i-1] == ')'
+            //                                      and s[pi] == '('
+            if (s.charAt(i) == ')') {
+                if (s.charAt(i - 1) == '(') { // found '()'
+                    dp[i] = (i >= 2 ? dp[i - 2] : 0) + 2;
                 } else { // str ends with '))'   ()((xxxxxx))
                     //                             ^        ^
-                    // s:                          pi      i-1
+                    // s:                          pi       i
                     //                            ^
-                    // dp:                      dp[pi]
-                    int pi = i - 1 - dp[i - 1] - 1;
+                    // dp:                      dp[pi-1]
+                    int pi = i - dp[i - 1] - 1;
                     if (pi >= 0 && s.charAt(pi) == '(') { // if that is '('
-                        // we can extend the length
-                        dp[i] = dp[i - 1] + dp[pi] + 2;
+                        // we extend the length
+                        dp[i] = dp[i - 1] + (pi > 0 ? dp[pi - 1] : 0) + 2;
                     }
                 }
             }
