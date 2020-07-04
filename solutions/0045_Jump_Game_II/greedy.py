2694f9d8269a9317062aabc2749fd621145c3fbd
zh-wang <viennakanon@gmail.com>
2020-07-04 21:09:27 +0900

    review

--- solutions/0045_Jump_Game_II/greedy.py
+++ solutions/0045_Jump_Game_II/greedy.py
@@ -3,14 +3,11 @@ class Solution:
         # let s(i) => step to nums[i], s(j) => step to nums[j]
         # if i < j, then s(i) <= s(j)
         # Prove by contradiction, assume that s(i) > s(j):
-        # There must be a common parent in both path i and path j,
-        # a. If it is positioned before i, we can rebuild both paths from it.
-        #    We can repeat above procedure.
-        #    Eventually, we can build a path reach i first.
-        #    This is the contradiction of s(i) > s(j)
-        # b. If it is positioned after i,
-        #    Then we can reach i by just 1 step.
-        #    This is the contradiction of s(i) > s(j)
+        # There must be a common parent in both path i and path j(e.g. nums[0])
+        # If we can reach j with s(j) steps, because of i < j, then we can reach
+        # i eariler than j. Therefore, s(i) <= s(j).
+        # This is the contradiction of s(i) < s(j).
+        #
         # Therefore, we can calc the max reach for each step by greedy algorithm.
         if len(nums) == 0: return 0
         i, j = 0, 0 # we can read nums from index i to j by 'step' steps
