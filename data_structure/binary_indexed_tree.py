b6d068d7aaf724852fd9a80d31c8df9033d93c2c
zh-wang <viennakanon@gmail.com>
2021-08-13 18:55:41 +0900

    update bineary indexed tree

--- data_structure/binary_indexed_tree.py
+++ data_structure/binary_indexed_tree.py
@@ -2,7 +2,8 @@ class BIT:
     def __init__(self, k): # init with size k
         self.maxIndex = k
         self._nums = [0] * self.maxIndex
-        self.tree = [0] * (self.maxIndex + 1) # index from 1, NOT 0
+        # index from 1, NOT 0
+        self.tree = [0] * (self.maxIndex + 1)
 
     def update(self, k, val):
         diff = val - self._nums[k]
@@ -22,23 +23,36 @@ class BIT:
         return ret
 
     def query_range(self, i, j):
-        return self.read(j) - (0 if not i else self.read(i - 1))
+        # return self.read(j) - (0 if not i else self.read(i - 1))
+        return self.read(j) - self.read(i)
 
-# bit = BIT(8)
-# for i in range(1, 9):
+bit = BIT(8)
+# for i in range(8):
     # bit.update(i, 1)
-# print(bit.read(1))
-# print(bit.read(4))
-# print(bit.query_range(1, 4))
-# print(bit.query_range(2, 4))
+bit.update(7, 1)
+
+print('read 0 : ', bit.read(0))
+print('read 1 : ', bit.read(1))
+print('read 4 : ', bit.read(4))
+print('read 7 : ', bit.read(7))
+print('query 0 ~ 4 : ', bit.query_range(0, 4))
+print('query 1 ~ 4 : ', bit.query_range(1, 4))
+print('query 2 ~ 4 : ', bit.query_range(2, 4))
+print('query 0 ~ 2 : ', bit.query_range(0, 2))
+bit.update(3, 1) # add 1 in range [3, 4]
+bit.update(5, -1)
+print('query 0 ~ 2 : ', bit.query_range(0, 2))
+print('query 0 ~ 3 : ', bit.query_range(0, 3))
+print('query 0 ~ 4 : ', bit.query_range(0, 4))
+print('query 0 ~ 5 : ', bit.query_range(0, 5))
 
 # ======= Number of swaps in Bubble Sort =========
-arr = [3, 5, 1, 2, 4, 7, 8, 6]
-t = BIT(9)
-# swap need for arr[i] = i - #(arr[j] < arr[i], which j < i)
-#                               ^ We use BIT to count this
-number_of_swaps = 0
-for i, v in enumerate(arr):
-    number_of_swaps += (i - t.read(v)) # t.read(v) => #(encounter of value < v)
-    t.update(v, 1) # add encounter of v
-    print(number_of_swaps)
+# arr = [3, 5, 1, 2, 4, 7, 8, 6]
+# t = BIT(9)
+# # swap need for arr[i] = i - #(arr[j] < arr[i], which j < i)
+# #                               ^ We use BIT to count this
+# number_of_swaps = 0
+# for i, v in enumerate(arr):
+    # number_of_swaps += (i - t.read(v)) # t.read(v) => #(encounter of value < v)
+    # t.update(v, 1) # add encounter of v
+    # print(number_of_swaps)
